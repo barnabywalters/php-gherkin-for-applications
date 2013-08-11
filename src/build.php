dae6c8659af9c5da5351385c73e5f7bbaa1545fd
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-08-11 13:00:44 +0000

    Got first command built, reading input and writing output

--- src/build.php
+++ src/build.php
@@ -6,4 +6,19 @@ require __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\Console;
 
 $app = new Console\Application;
+
+$app->register('build')
+	->setDefinition([
+		new Console\Input\InputArgument('dir', Console\Input\InputArgument::OPTIONAL, 'Project Folder', 'CURRENT_DIRECTORY')
+	])
+	->setDescription('Packages a .phar file from a project directory (current dir by default)')
+	->setCode(function (Console\Input\InputInterface $input, Console\Output\OutputInterface $output) {
+		$dir = $input->getArgument('dir');
+		
+		if ($dir == 'CURRENT_DIRECTORY')
+			$dir = getcwd();
+		
+		$output->writeln("Building project from {$dir}");
+	});
+
 $app->run();
